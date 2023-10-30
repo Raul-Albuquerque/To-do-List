062a600cab9060fa4f628cab896390d1ed232b71
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2023-10-30 15:33:53 -0300

    aplicações de funcionalidades finais

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import { Provider } from 'react-redux'
-import BarraLateral from './containers/BarraLateral'
-import ListaDeTarefas from './containers/ListaDeTarefas'
-import EstiloGlobal, { Container } from './styles'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import EstiloGlobal, { Container } from './styles'
+import Home from './pages/Home'
+import Cadastro from './pages/Cadastro'
 import store from './store'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/novo',
+    element: <Cadastro />
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <EstiloGlobal />
       <Container>
-        <BarraLateral />
-        <ListaDeTarefas />
+        <RouterProvider router={rotas} />
       </Container>
     </Provider>
   )
--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,52 +1,66 @@
 import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
 import FiltroCard from '../../components/FiltroCard'
 import * as S from './styles'
 import { RootReducer } from '../../store'
 import { alterarTermo } from '../../store/reducers/filtro'
 import * as enums from '../../utils/enums/Tarefa'
+import { Botao, Campo } from '../../styles'
 
-const BarraLateral = () => {
+type Props = {
+  mostrarFiltros: boolean
+}
+
+const BarraLateral = ({ mostrarFiltros }: Props) => {
+  const navigate = useNavigate()
   const dispatch = useDispatch()
   const { termo } = useSelector((state: RootReducer) => state.filtro)
 
   return (
     <S.Aside>
-      <div>
-        <S.Campo
-          type="text"
-          placeholder="Buscar"
-          value={termo}
-          onChange={(evento) => dispatch(alterarTermo(evento.target.value))}
-        />
-        <S.Filtros>
-          <FiltroCard
-            valor={enums.Status.PENDENTE}
-            criterio="status"
-            legenda="pendentes"
+      {mostrarFiltros ? (
+        <div>
+          <Campo
+            type="text"
+            placeholder="Buscar"
+            value={termo}
+            onChange={(evento) => dispatch(alterarTermo(evento.target.value))}
           />
-          <FiltroCard
-            valor={enums.Status.CONCLUIDA}
-            criterio="status"
-            legenda="concluidas"
-          />
-          <FiltroCard
-            valor={enums.Prioridade.URGENTE}
-            criterio="prioridade"
-            legenda="urgentes"
-          />
-          <FiltroCard
-            valor={enums.Prioridade.IMPORTANTE}
-            criterio="prioridade"
-            legenda="importantes"
-          />
-          <FiltroCard
-            valor={enums.Prioridade.NORMAL}
-            criterio="prioridade"
-            legenda="normal"
-          />
-          <FiltroCard criterio="todas" legenda="todas" />
-        </S.Filtros>
-      </div>
+          <S.Filtros>
+            <FiltroCard
+              valor={enums.Status.PENDENTE}
+              criterio="status"
+              legenda="pendentes"
+            />
+            <FiltroCard
+              valor={enums.Status.CONCLUIDA}
+              criterio="status"
+              legenda="concluidas"
+            />
+            <FiltroCard
+              valor={enums.Prioridade.URGENTE}
+              criterio="prioridade"
+              legenda="urgentes"
+            />
+            <FiltroCard
+              valor={enums.Prioridade.IMPORTANTE}
+              criterio="prioridade"
+              legenda="importantes"
+            />
+            <FiltroCard
+              valor={enums.Prioridade.NORMAL}
+              criterio="prioridade"
+              legenda="normal"
+            />
+            <FiltroCard criterio="todas" legenda="todas" />
+          </S.Filtros>
+        </div>
+      ) : (
+        <Botao onClick={() => navigate('/')} type="button">
+          Voltar a lista de tarefas
+        </Botao>
+      )}
     </S.Aside>
   )
 }
